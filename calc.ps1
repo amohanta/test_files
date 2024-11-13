967d58be1a1bae2fad96fae8fa56853d94f73a1d
abhijit mohanta <abhijit.mohanta.15.08@gmail.com>
2024-11-13 12:12:23 +0530

    Update calc.ps1

--- calc.ps1
+++ calc.ps1
@@ -1,3 +1,6 @@
 # Set execution policy to Bypass and execute Windows Calculator
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 Start-Process calc.exe
+do {
+    $input = Read-Host "Enter 1 to exit"
+} while ($input -ne "1")  # Loop continues until the user enters "1"
