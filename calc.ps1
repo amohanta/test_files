967c3ded9d757e400dd0ccfb9455510ec49805ba
abhijit mohanta <abhijit.mohanta.15.08@gmail.com>
2024-11-13 12:21:20 +0530

    Update calc.ps1

--- calc.ps1
+++ calc.ps1
@@ -1,5 +1,6 @@
 # Set execution policy to Bypass and execute Windows Calculator
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
+Write-Output "FileLessAttackDemo"
 Start-Process calc.exe
 do {
     $input = Read-Host "Enter 1 to exit"
